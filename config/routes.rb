7c844364d6c42635eb64a4b1acab5bf8a609c3ed
Lawrence Secrease <lawrence@bookshout.com>
2017-01-30 15:00:49 -0600

    User model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/edit'
-
-  get 'users/profile'
-
-  get 'users/matches'
+#  get 'users/index'
+#
+#  get 'users/edit'
+#
+#  get 'users/profile'
+#
+#  get 'users/matches'
 
   root 'home#index'
+ 
+  resources :users do
+   member do
+    get 'profile'
+    get 'matches'
+   end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
