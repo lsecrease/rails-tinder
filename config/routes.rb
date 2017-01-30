13acce8118f04dace43ece799658eef3c2c53255
Lawrence Secrease <lawrence@bookshout.com>
2017-01-30 15:24:57 -0600

    FB Login done

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     get 'matches'
    end
   end
+ 
+ get 'auth/:provider/callback', to: 'sessions#create'
+ match 'sign_out', to: 'sessions#destroy', via: :delete
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
